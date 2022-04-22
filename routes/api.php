a2aa0f99cb43a145300dc56d942cf8081ce30e71
Teph Gab <tephgab@gmail.com>
2022-04-22 16:26:36 -0400

    remove middleware

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,10 @@ use App\Http\Controllers\CounterController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::resource('counter', CounterController::class);
+
+// Route::middleware('auth:api')->
