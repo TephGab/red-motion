6978f62bef0667bafbee4d45e219aa435ada3b86
Teph Gab <tephgab@gmail.com>
2022-07-31 03:09:39 -0400

    Https 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if(config('app.env') == 'production'){
-            \URL::forceScheme('https');
-        }
+        if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        } 
+        // if(config('app.env') == 'production'){
+        //     \URL::forceScheme('https');
+        // }
     }
 
     /**
